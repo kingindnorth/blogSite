5a68c51ae384023244b845f6d91f721de94f6201
prajjawal tiwari <kingindnorth.0311@gmail.com>
2022-12-15 15:35:19 +0530

    auth

---
+++ controllers/auth.js
@@ -0,0 +1,63 @@
+const bcrypt = require("bcryptjs")
+const User = require("../models/User")
+
+const register = async(req,res) => {
+    try{
+        const salt = await bcrypt.genSalt(10)
+        const hassPassword = await bcrypt.hash(req.body.password, salt)
+        const user = new User({
+            username:req.body.username,
+            email:req.body.email,
+            password:hassPassword
+        })
+
+        const newUser = await user.save()
+        res.status(200).json({
+            success:true,
+            msg:"user saved successfully",
+            payload:newUser
+        })
+
+    }catch(err){
+        console.log(err)
+        res.status(500).json({
+            success:false,
+            msg:"internal server error"
+        })
+    }
+}
+
+const login = async(req,res) => {
+    try{
+        const user = await User.find({username:req.body.username})
+        if(!user) return res.status(400).json({
+            success:false,
+            msg:"Enter a valid username"
+        })
+        
+        const validate = await bcrypt.compare(req.body.password, user.password)
+        if(!validate) return res.status(400).json({
+            success:false,
+            msg:"Incorrect password"
+        })
+
+        const {password, ...others} = user._doc
+        res.status(200).json({
+            success:true,
+            msg:"You are now loged in!",
+            payload:others
+        })
+
+    }catch(err){
+        console.log(err)
+        res.status(500).json({
+            success:false,
+            msg:"internal server error"
+        })
+    }
+}
+
+module.exports = {
+    register,
+    login
+}
--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,11 @@
 const router = require("express").Router()
 
-router.get("/",)
+const {
+    register,
+    login
+} = require("../controllers/auth")
+
+router.post("/",register)
+router.post("/",login)
 
 module.exports = router
